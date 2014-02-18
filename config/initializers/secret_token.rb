3682208caaffbeac0c14f3ce561098ac0297ac7e
Zinn <Zinn@Livings-MacBook-Pro-77.local>
2014-02-18 13:43:34 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GruntlyDC::Application.config.secret_key_base = '249393a553dc01afbabe9373de6e13db81a060327d2629de49c17d3a913809346cc193db8297f53a7fa110f84c8c923350474a27e6d236f679eef13d5636303c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GruntlyDC::Application.config.session_store :cookie_store, key: '_GruntlyDC_session'
